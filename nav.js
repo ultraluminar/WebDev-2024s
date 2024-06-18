07f18b7013c6c9eefdb4547f8cea1d65b0727d81
KlasL4243 <lukasklassen4243@gmail.com>
2024-06-18 12:03:16 +0200

    chore: make script module and remove redundant defer

--- nav.js
+++ nav.js
@@ -1,9 +1,7 @@
-document.addEventListener("DOMContentLoaded", async function () {
-    const icon = document.getElementById("burger-menu");
-    const nav = document.getElementById("mobile-side-nav");
+const icon = document.getElementById("burger-menu");
+const nav = document.getElementById("mobile-side-nav");
 
-    icon.addEventListener("click", () => {
-        icon.classList.toggle('open');
-        nav.classList.toggle('open');
-    });
+icon.addEventListener("click", () => {
+    icon.classList.toggle('open');
+    nav.classList.toggle('open');
 });
