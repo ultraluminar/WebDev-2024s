c9182b9f87f420748af6a0149757c0d06bf5d17d
KlasL4243 <lukasklassen4243@gmail.com>
2024-06-18 14:55:28 +0200

    refactor: shorten file by using create function

--- shop/product-card.js
+++ shop/product-card.js
@@ -1,68 +1,44 @@
 export class ProductCard {
     constructor(parent, id, img_src, img_alt, title, description, price, cart){
-        const card = document.createElement("div");
+        const card = create("div", parent, "product-card")
         card.id = id;
-        card.classList.add("product-card");
-        parent.appendChild(card);
 
-            const image = document.createElement("img");
+            const image = create("img", card, "product-image");
             image.src = img_src;
-            image.classList.add("product-image");
-            image.alt = img_alt
-            card.appendChild(image);
+            image.alt = img_alt;
 
-            const info = document.createElement("div");
-            info.classList.add("product-info");
-            card.appendChild(info);
+            const info = create("div", card, "product-info");
 
-                const heading = document.createElement("h3");
-                heading.classList.add("product-title");
+                const heading = create("h3", info, "product-title");
                 heading.textContent = title;
-                info.appendChild(heading);
 
-                const paragraph = document.createElement("p");
-                paragraph.classList.add("product-description");
+                const paragraph = create("p", info, "product-description");
                 paragraph.textContent = description;
-                info.appendChild(paragraph);
 
-                const price_tag = document.createElement("p");
-                price_tag.classList.add("product-price");
+                const price_tag = create("p", info, "product-price");
                 price_tag.textContent = price;
-                info.appendChild(price_tag);
 
-            const buttons = document.createElement("div");
-            buttons.classList.add("product-buttons");
-            card.appendChild(buttons);
+            const buttons = create("div", card, "product-buttons");
 
-                const spinbox = document.createElement("div");
-                spinbox.classList.add("custom-spinbox");
-                buttons.appendChild(spinbox);
+                const spinbox = create("div", buttons, "custom-spinbox");
 
-                    const button_decrement = document.createElement("button");
-                    button_decrement.classList.add("spinbox-button", "spinbox-decrement");
-                    spinbox.appendChild(button_decrement);
+                    const button_decrement = create("button", spinbox, "spinbox-button");
 
-                        const minus = document.createElement("img");
+                        const minus = create("img", button_decrement);
                         minus.src = "../assets/shop/minus.svg";
-                        image.alt = "Minus Symbol";
-                        button_decrement.appendChild(minus);
+                        minus.alt = "Minus Symbol";
 
-                    const input = document.createElement("input");
+                    const input = create("input", spinbox, "spinbox-input");
                     input.type = "number";
-                    input.classList.add("spinbox-input");
                     input.min = 1;
                     input.max = 99;
                     input.value = 1;
-                    spinbox.appendChild(input);
 
-                    const button_increment = document.createElement("button");
-                    button_increment.classList.add("spinbox-button", "spinbox-increment");
-                    spinbox.appendChild(button_increment);
+                    const button_increment = create("button", spinbox, "spinbox-button");
 
-                        const plus = document.createElement("img");
+                        const plus = create("img", button_increment);
                         plus.src = "../assets/shop/plus.svg";
-                        image.alt = "Plus Symbol";
-                        button_increment.appendChild(plus);
+                        plus.alt = "Plus Symbol";
 
                     button_decrement.addEventListener('click', () => {
                         input.value = Math.max(parseInt(input.value) - 1, 1);
@@ -77,14 +53,19 @@ export class ProductCard {
                         else if (parseInt(input.value) > 99) input.value = 99;
                     });
 
-                const button_cart = document.createElement("button");
-                button_cart.classList.add("add-cart-button");
+                const button_cart = create("button", buttons, "add-cart-button");
                 button_cart.textContent = "In den Warenkorb";
-                buttons.appendChild(button_cart);
 
                 button_cart.addEventListener("click", () => {
                     cart.add(id, parseInt(input.value));
                     cart.save();
                 });
     }
+}
+
+function create(tagName, parent, _class){
+    const element = document.createElement(tagName);
+    if (_class !== undefined) element.classList.add(_class);
+    parent.appendChild(element);
+    return element;
 }
