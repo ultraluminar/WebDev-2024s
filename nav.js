1b9aadc8fdbd8c63c787f2cd60d73d924b01456a
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-06-19 17:33:03 +0200

    feat: add mobile nav filter

--- nav.js
+++ nav.js
@@ -1,7 +1,17 @@
 const icon = document.getElementById("burger-menu");
 const nav = document.getElementById("mobile-side-nav");
+const filter = document.getElementById("mobile-nav-filter");
 
-icon.addEventListener("click", () => {
+function toggleNav() {
     icon.classList.toggle('open');
     nav.classList.toggle('open');
+    filter.classList.toggle('open');
+}
+
+icon.addEventListener("click", () => {
+    toggleNav();
+});
+
+filter.addEventListener("click", () => {
+    toggleNav();
 });
