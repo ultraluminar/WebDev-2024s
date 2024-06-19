b6bfeecc949643f69485d295ba804a2bdda23aa1
KlasL4243 <lukasklassen4243@gmail.com>
2024-06-19 09:35:31 +0200

    refactor: use deconstructed object values as parameters

--- shop/script.js
+++ shop/script.js
@@ -4,13 +4,5 @@ import { product_details } from "./product_details.js";
 const parent = document.querySelector("#merch").querySelector(".section-body");
 
 product_details.forEach((product) => {
-    createCard(
-        parent,
-        product.id,
-        product.title,
-        product.description,
-        `${product.price}€`,
-        product.img_src,
-        product.img_alt
-    )
+    createCard(parent, ...Object.values(product));
 });
